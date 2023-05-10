98767e0fd9973e533867ef289bfcebf7c4fddccc
Farid Isayev <faridisayev3@icloud.com>
2023-05-10 10:10:38 +0300

    Add Remove Item button

--- app.py
+++ app.py
@@ -23,27 +23,26 @@ def cart():
         session["cart"] = {}
 
     if request.method == "POST":
-        id = int(request.form.get("id"))
+        id, quantity = int(request.form.get("id")), int(request.form.get("quantity"))
+
         if id:
-            if id in session["cart"]:
-               session["cart"][id]["quantity"] += 1
-            else:
-               session["cart"][id] = { "quantity":1 }
+            session_quantity = session["cart"].get(id, {"quantity": 0})["quantity"]
+            quantity = min(quantity, 10 - session_quantity)
+            session["cart"][id] = {"quantity": session_quantity + quantity}
         return index(id)
+    
     cart = db.execute("SELECT * FROM books WHERE id IN(?)", list(session["cart"].keys()))
 
     for item in cart:
         item['quantity'] = session["cart"][int(item['id'])]["quantity"]
-
     return render_template('cart.html', stylesheet = url_for('static', filename='styles/cart.css'), cart=cart)
 
 @app.route("/remove", methods=["POST"])
 def remove():
     id = int(request.form.get("id"))
-    if id:
-        session["cart"][id]["quantity"] -= 1
-        if session["cart"][id]["quantity"] <= 0:
-            del session["cart"][id]
 
+    if id:
+        session["cart"][id]["quantity"] -= 1 if "decrease-quantity" in request.form else 0
+        if session["cart"][id]["quantity"] <= 0 or "remove-item" in request.form:
+            del session["cart"][id]
     return redirect("/cart")
-    
